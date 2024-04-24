27453727b119e09bb3c5bf1acddf099e21db617c
NandiniYalamanchi <nandiniyalamanchi986@gmail.com>
2024-04-24 19:38:36 +0530

    Updated my name

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,10 @@ const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold text-blue-500">
-      Hello world!
+      Hello Nandini..!
       </h1>
     </>
   );
 }
 
-export default App;
-
+export default App; 
