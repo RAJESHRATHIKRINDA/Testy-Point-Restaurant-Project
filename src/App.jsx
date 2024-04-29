fb49c5913df1a43b3fb1f11512adc092cffd2a81
aishwaryapinky <aishwaryapinky5052@gmail.com>
2024-04-29 20:47:34 +0530

    updated my name

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <h1 className='text-3xl font-bold text-blue-500'>
-        Hello Nandini..!
+        Hello Aishwarya...!
         {/* routers */}
         <Test />
       </h1>
